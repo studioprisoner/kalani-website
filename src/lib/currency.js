2c1ee33563bacc2f6224750d720cab8751c58611
Josh Illichmann <josh@studioprisoner.com>
2021-02-24 14:44:45 +1100

    fixed locale info

--- src/lib/currency.js
+++ src/lib/currency.js
@@ -6,7 +6,7 @@ export function formatCurrency(number) {
     if ( typeof number !== 'number' ) {
       throw new Error(`Failed to format currency; Invalid number type ${typeof number}`);
     }
-    return (number).toLocaleString('en-AUD', {
+    return (number).toLocaleString('en-AU', {
       style: 'currency',
       currency: 'AUD'
     })
