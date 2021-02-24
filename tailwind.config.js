dfd5887c03132dc13af1a848c644841ea9b2d42b
Josh Illichmann <josh@studioprisoner.com>
2021-02-24 14:15:44 +1100

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+    './src/**/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
