e751434d48200c8e10b69ed85e581f9e8205e585
Josh Illichmann <josh@studioprisoner.com>
2021-03-07 14:03:29 +1100

    udpated to have pop up for no items in the cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -120,16 +120,48 @@ export default function Home() {
             <div className="shadow border-b border-gray-200 sm:rounded-lg">
               <Table data={data} columns={columns} />
             </div>
-            <div className="">
+            <div className="bg-white overflow-hidden shadow rounded-lg mt-3">
             {data.length > 0 && (
-                  <p>
+                  <div>
                     <button className="mt-8 block w-full bg-almond-300 border border-almond-300 rounded-md py-2 text-sm font-semibold text-rose-bud-500 text-center hover:bg-almond-500 hover:border-almond-500" onClick={handleOnCheckout}>Check Out with Stripe</button>
-                  </p>
+                  </div>
             )}
             {data.length === 0 && (
-              <p className="my-6 text-base text-gray-500">
-                No items in your cart. <Link href="/"><a className="text-base font-semibold underline text-mongoose-500 hover:text-mongoose-700">Go add something</a></Link>!
-              </p>
+            <div class="fixed z-10 inset-0 overflow-y-auto">
+              <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                  <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                </div>
+                <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                  <div class="sm:flex sm:items-start">
+                    <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                      </svg>
+                    </div>
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                        No Products
+                      </h3>
+                      <div class="mt-2">
+                        <p class="text-sm text-gray-500">
+                          There are no products in your cart, go and add some.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                    <Link href="/products">
+                    <a class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-apricot-peach-500 text-base font-medium text-white hover:bg-apricot-peach-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-bud-500 sm:ml-3 sm:w-auto sm:text-sm">
+                      Find Products
+                    </a>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             )}
           </div>
           </div>
