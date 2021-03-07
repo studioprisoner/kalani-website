80c8c4f46c19d5d678793a7853eb7268d42425ed
Josh Illichmann <josh@studioprisoner.com>
2021-03-07 13:45:16 +1100

    fixed href in Link elemetn

--- src/pages/success.js
+++ src/pages/success.js
@@ -42,7 +42,7 @@ export default function Success() {
                     </div>
                 </div>
                 <div className="mt-5 sm:mt-6">
-                    <Link>
+                    <Link href="/">
                     <a className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm">
                     Go home.
                     </a>
