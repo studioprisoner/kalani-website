509f1f9e54e503849fb8e085fbf75dd4a32dcbff
Josh Illichmann <josh@studioprisoner.com>
2021-03-22 15:01:56 +1100

    change back to prod API

--- src/pages/api/subscribe_new.js
+++ src/pages/api/subscribe_new.js
@@ -1,13 +1,13 @@
 const sgMail = require('@sendgrid/mail')
 
 export default async function(req, res) {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY_JOSH)
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
   const { email } = req.body
 
   const content = {
-       to: 'josh@studioprisoner.com',
-       from: 'josh@studioprisoner.com',
+       to: 'jordyn@kalani-co.com.au',
+       from: 'jordyn@kalani-co.com.au',
        subject: `New Subscriber to Newsletter`,
        html: `<p>Please add this email address: ${email} to the newsletter database`
     }
